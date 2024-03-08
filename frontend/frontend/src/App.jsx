e93153ea25421223c5e15fc9135f81eed99a66b1
Raze <ahsan.yousaf1828@gmail.com>
2024-03-08 17:30:33 +0500

    Update App.jsx

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import NavBar from './components/NavBar'
+// Desc: Main App file
+
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import "@fontsource/poppins";
 
 import './output.css'
-import HeroSection from './components/HeroSection'
 import Contact from './Pages/Contact'
 import Vehicle from './Pages/Vehicle'
 import Homepage from './Pages/Homepage'
-import Footer from './components/Footer'
+
 import LoginPage from './Pages/LoginPage'
 
 import Dashboard from './components/DashboardMain'
