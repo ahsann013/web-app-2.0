6200a80c4f0bd63ab96799e30b85c02a1d29ea73
Raze <ahsan.yousaf1828@gmail.com>
2024-03-17 17:47:02 +0500

    commit

--- frontend/frontend/src/components/CarImage.jsx
+++ frontend/frontend/src/components/CarImage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CarImage = () => {
   return (
     <div className="flex justify-center items-center h-80 bg-gradient-to-br from-amber-600 to-amber-500 rounded-full shadow-cl">
-      <img src="./src/assets/bike-logo.svg" alt="carimage" className="h-80 w-80 rounded-full border-4 border-white " />
+      <img src='./public/bike-logo.svg' alt="carimage" className="h-80 w-80 rounded-full border-4 border-white " />
     </div>
   );
 };
